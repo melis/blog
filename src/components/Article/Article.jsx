a51a81dd3affd91aa3e65509a58ffa1c42c2be7d
Melis <59087686+melis@users.noreply.github.com>
2020-10-15 20:25:15 +0600

    ar

--- src/components/Article/Article.jsx
+++ src/components/Article/Article.jsx
@@ -1,8 +1,12 @@
 import React from "react";
-
+import style from "./Article.module.scss";
 const Article = (props) => {
   console.log(props);
   const { article } = props;
-  return <div>{article.slug}</div>;
+  return (
+    <div className={style.article}>
+      {article.author.username} {article.slug}
+    </div>
+  );
 };
 export default Article;
