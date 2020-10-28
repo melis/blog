34eda7054c5f51dd59cb3b465fa36a15121a474c
Melis <59087686+melis@users.noreply.github.com>
2020-10-29 02:48:26 +0600

    sss

--- src/components/EditArticle/EditArticle.js
+++ src/components/EditArticle/EditArticle.js
@@ -4,10 +4,12 @@ import PropTypes from 'prop-types';
 import style from './EditArticle.module.scss';
 import * as actions from '../../store/slugActions';
 import ArticleForm from '../EditArticleForm/EditArticleForm';
+import { withRouter } from 'react-router-dom';
 
-const EditArticle = props => {
+const EditArticle = (props) => {
   const { user, match, setSlug, updSlug, slug } = props;
   const { slugName } = match.params;
+  console.log(user, slug);
 
   useEffect(() => {
     setSlug(slugName, user.token);
@@ -16,11 +18,13 @@ const EditArticle = props => {
   return (
     <div className={style.article}>
       <h1>Edit article</h1>
-      <ArticleForm token={user.token} submit={updSlug} slug={slug} />
+      {slug && slug.author.username === user.username ? (
+        <ArticleForm token={user.token} submit={updSlug} slug={slug} />
+      ) : null}
     </div>
   );
 };
-const mapStateToProps = state => {
+const mapStateToProps = (state) => {
   return {
     user: state.user.user,
     slug: state.slug.slug,
@@ -33,4 +37,4 @@ EditArticle.propTypes = {
   updSlug: PropTypes.func,
   slug: PropTypes.object,
 };
-export default connect(mapStateToProps, actions)(EditArticle);
+export default withRouter(connect(mapStateToProps, actions)(EditArticle));
